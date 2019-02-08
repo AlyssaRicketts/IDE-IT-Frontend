207280647bdc65b5e9efb9e71cc9afc07a95076b
Puja Ramanathan <puja.ramanathan@gmail.com>
2019-02-08 11:59:29 -0800

    Added label and lightbulb.png

--- HelloWorld/src/helloworld/views/MainView.java
+++ HelloWorld/src/helloworld/views/MainView.java
@@ -1,27 +1,48 @@
 package helloworld.views;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.part.ViewPart;
 
-public class MainView extends ViewPart {
+import main.java.Suggestion;
 
+public class MainView extends ViewPart {
+	private static final int CONFIG = 0;
+	private static final int HOTKEY = 1;
 	private Button button;
+	private Label hotkey;
+	private Composite thisParent;
+	private Suggestion firstSuggestion;
 
     public MainView() {
         super();
+        firstSuggestion = new Suggestion("Try using 'CMD + /' to comment several lines.", HOTKEY, true);
     }
 
     @Override
     public void createPartControl(Composite parent) {
-        button = new Button(parent,SWT.CHECK);
-        button.setText("An example of a checkbox");
+    	thisParent = parent;
+        createHotkeyTip(firstSuggestion);
     }
 
     @Override
     public void setFocus() {
-        button.setFocus();
+        hotkey.setFocus();
+    }
+    
+    public void createHotkeyTip(Suggestion s) {
+        // button = new Button(thisParent, SWT.CHECK);
+        // button.setText(s.getText());
+        
+        // Display display = new Display();
+        // Image image = new Image(display, "../../../icons/lighbulb.png");
+        hotkey = new Label(thisParent, 0);
+        // hotkey.setImage(image);
+        hotkey.setText(s.getText());
     }
 
 }
--- HelloWorld/src/main/java/Controller.java
+++ HelloWorld/src/main/java/Controller.java
@@ -3,12 +3,18 @@ package main.java;
 import java.util.HashMap;
 import java.util.Map;
 
+import helloworld.views.MainView;
+
 public class Controller {
 	private static final int CONFIG = 0;
 	private static final int HOTKEY = 1;
 	
-	Map<String, Suggestion> idsToSuggestions = new HashMap<String, Suggestion>();
+	Map<String, Suggestion> idsToSuggestions;
 	
-	Suggestion multilineComment = new Suggestion("Try using 'CMD + /' to comment several lines.", HOTKEY, true);
+	public Controller() {
+		idsToSuggestions = new HashMap<String, Suggestion>();
+		Suggestion multilineComment = new Suggestion("Try using 'CMD + /' to comment several lines.", HOTKEY, true);
+		idsToSuggestions.put("multiline comment", multilineComment);
+	}
 
 }
