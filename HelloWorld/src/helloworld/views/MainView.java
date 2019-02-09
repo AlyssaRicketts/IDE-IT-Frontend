b966dc361f0690318950de7540971a6098f3f801
rzigman <41302563+rzigman@users.noreply.github.com>
2019-02-09 10:05:46 -0800

    added icon to label, map for suggestions

--- HelloWorld/src/helloworld/views/MainView.java
+++ HelloWorld/src/helloworld/views/MainView.java
@@ -1,11 +1,16 @@
 package helloworld.views;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 import main.java.Suggestion;
@@ -14,19 +19,27 @@ public class MainView extends ViewPart {
 	private static final int CONFIG = 0;
 	private static final int HOTKEY = 1;
 	private Button button;
-	private Label hotkey;
+	private CLabel hotkey;
 	private Composite thisParent;
-	private Suggestion firstSuggestion;
+	private Map<String, Suggestion> suggestionsMap = new HashMap<String, Suggestion>();
 
     public MainView() {
         super();
-        firstSuggestion = new Suggestion("Try using 'CMD + /' to comment several lines.", HOTKEY, true);
+        suggestionsMap.put("firstSuggestion", new Suggestion("Try using 'CMD + /' to comment several lines.", HOTKEY, true));
+        suggestionsMap.put("secondSuggestion", new Suggestion("Enable autocomplete", CONFIG, true, false));
+        
     }
 
     @Override
     public void createPartControl(Composite parent) {
     	thisParent = parent;
-        createHotkeyTip(firstSuggestion);
+    	for (Suggestion s: suggestionsMap.values()) {
+    		if (s.type == CONFIG) {
+    			createConfigTip(s);
+    		} else {
+    			createHotkeyTip(s);
+    		}
+    	}
     }
 
     @Override
@@ -35,14 +48,22 @@ public class MainView extends ViewPart {
     }
     
     public void createHotkeyTip(Suggestion s) {
-        // button = new Button(thisParent, SWT.CHECK);
-        // button.setText(s.getText());
+    	Display display = PlatformUI.getWorkbench().getDisplay();
         
-        // Display display = new Display();
-        // Image image = new Image(display, "../../../icons/lighbulb.png");
-        hotkey = new Label(thisParent, 0);
-        // hotkey.setImage(image);
+        Image image = new Image(display, getClass().getResourceAsStream("lightbulb.png"));
+        
+        hotkey = new CLabel(thisParent, 0);
+        
+        hotkey.setImage(image);
         hotkey.setText(s.getText());
+        
     }
+    
+    public void createConfigTip(Suggestion s) {
+    	button = new Button(thisParent, SWT.CHECK);
+    	button.setText(s.getText());
+    }
+    
+
 
 }
