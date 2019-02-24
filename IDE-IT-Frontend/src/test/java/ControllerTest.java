547441bd21f5fb272a9c1b24b7987f13b3ac7747
Alyssa <alyssr3@cs.washington.edu>
2019-02-24 12:42:53 -0800

    Add OS Info class and use OS Info to inform hotkey tips

--- IDE-IT-Frontend/src/test/java/ControllerTest.java
+++ IDE-IT-Frontend/src/test/java/ControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import main.java.Controller;
+import main.java.OSInfo;
 import main.java.Suggestion;
 
 public class ControllerTest {
@@ -25,13 +26,20 @@ public class ControllerTest {
 	public void testMap() {
 		Map<String, Suggestion> suggestionsMap = new HashMap<String, Suggestion>();
 		
-		// Hotkeys
-        suggestionsMap.put("blockCommentSuggestion", new Suggestion("blockCommentSuggestion", "Try using 'CMD + /' to comment several lines.", HOTKEY, true));
-        suggestionsMap.put("addImportStatementsSuggestion", new Suggestion("addImportStatementsSuggestion", "Try using 'Ctrl + Shift + O' to add import statements.", HOTKEY, true));
-        suggestionsMap.put("removeUnusedImportsStatementSuggestion", new Suggestion("removeUnusedImportsStatementSuggestion", "Try using 'Ctrl + Shift + O' to remove unused imports.", HOTKEY, true));
-        suggestionsMap.put("correctIndentationsSuggestion", new Suggestion("correctIndentationsSuggestion", "Try using 'Ctrl + i' to correct indentation.", HOTKEY, true));
-        suggestionsMap.put("variableRenameRefactorSuggestion", new Suggestion("variableRenameRefactorSuggestion", "Try using 'Alt + Shift + R' to rename all instances of a variable, class, or method.", HOTKEY, true));
-        
+		// Hotkeys that vary between operating systems
+		if(OSInfo.isMac() || OSInfo.isUnix() ) {
+	        suggestionsMap.put("blockCommentSuggestion", new Suggestion("blockCommentSuggestion", "Try using 'CMD + /' to comment several lines.", HOTKEY, true));
+	        suggestionsMap.put("variableRenameRefactorSuggestion", new Suggestion("variableRenameRefactorSuggestion", "Try using 'CMD + OPTION + R' to rename all instances of a variable, class, or method.", HOTKEY, true));
+		} else {  // Windows
+	        suggestionsMap.put("blockCommentSuggestion", new Suggestion("blockCommentSuggestion", "Try using 'CTRL + SHIFT + /' to comment several lines.", HOTKEY, true));
+	        suggestionsMap.put("variableRenameRefactorSuggestion", new Suggestion("variableRenameRefactorSuggestion", "Try using 'ALT + SHIFT + R' to rename all instances of a variable, class, or method.", HOTKEY, true));
+		}
+		
+		// Hotkeys that remain the same between operating systems
+		suggestionsMap.put("addImportStatementsSuggestion", new Suggestion("addImportStatementsSuggestion", "Try using 'CTRL + SHIFT + O' to add import statements.", HOTKEY, true));
+        suggestionsMap.put("removeUnusedImportsStatementSuggestion", new Suggestion("removeUnusedImportsStatementSuggestion", "Try using 'CTRL + SHIFT + O' to remove unused imports.", HOTKEY, true));
+        suggestionsMap.put("correctIndentationsSuggestion", new Suggestion("correctIndentationsSuggestion", "Try using 'CTRL + I' to correct indentation.", HOTKEY, true));
+		
         // Configurations
         suggestionsMap.put("enableAutocompleteSuggestion", new Suggestion("enableAutocompleteSuggestion", "Enable content assist auto activation", CONFIG, true, false));
         suggestionsMap.put("enableSmartSemicolonSuggestion", new Suggestion("enableSmartSemicolonSuggestion", "Enable smart semicolon activation", CONFIG, true, false));
