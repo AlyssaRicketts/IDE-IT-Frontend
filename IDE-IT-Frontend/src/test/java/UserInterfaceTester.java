df7113f7669e65bb1603781b53c2417a588250e7
Alyssa <alyssr3@cs.washington.edu>
2019-03-04 10:36:46 -0800

    Add multiline comment test

--- IDE-IT-Frontend/src/test/java/UserInterfaceTester.java
+++ IDE-IT-Frontend/src/test/java/UserInterfaceTester.java
@@ -1,5 +1,6 @@
 package test.java;
 
+import org.junit.runners.MethodSorters;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -7,8 +8,11 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEclipseEditor;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.utils.FileUtils;
 import org.eclipse.swtbot.swt.finder.waits.Conditions;
 import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
 import org.eclipse.swtbot.swt.finder.waits.ICondition;
@@ -19,10 +23,15 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.osgi.framework.Bundle;
+
+import activation.Activator;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserInterfaceTester {
 	private static SWTWorkbenchBot bot;
 	private static SWTBotView ideitView;
@@ -32,15 +41,18 @@ public class UserInterfaceTester {
 		bot = new SWTWorkbenchBot();
 		openJavaPerspective();
 		closeWelcomePage();
-		openIDEITWindow();
 		
-		// Must set up preferences in workspace, to what the workspace typically launches as
+		/*// Must set up preferences in workspace, to what the workspace typically launches as
 		IEclipsePreferences JDTUIPrefs = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.ui");
 		JDTUIPrefs.put("content_assist_autoactivation", "false");
 		JDTUIPrefs.put("smart_semicolon", "false");
 		
 		IEclipsePreferences JDTCorePrefs = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.core");
-		JDTCorePrefs.put("org.eclipse.jdt.core.compiler.problem.fieldHiding", "ignore");
+		JDTCorePrefs.put("org.eclipse.jdt.core.compiler.problem.fieldHiding", "ignore");*/
+		
+		openIDEITWindow();
+		createANewJavaProject();
+		createANewJavaClass();
 	}
 	
 	@AfterClass
@@ -49,33 +61,63 @@ public class UserInterfaceTester {
     }
 	
 	@Test
-	public void testOpenIDEITWindow() {
+	public void test1_testOpenIDEITWindow() {
 		assertNotNull(bot.viewByTitle("IDE-IT"));
 	}
 	
     @Test
-    public void testContentAssistAutoActivation() {
+    public void test2_testContentAssistAutoActivation() {
     	IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.ui");
+    	prefs.put("content_assist_autoactivation", "false");
     	assertTrue(prefs.get("content_assist_autoactivation", "default").equals("false"));
         bot.checkBox("Enable content assist auto activation").click();
         assertTrue(prefs.get("content_assist_autoactivation", "default").equals("true"));
     }
     
     @Test
-    public void testSmartSemicolon() {
+    public void test3_testSmartSemicolon() {
     	IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.ui");
+    	prefs.put("smart_semicolon", "false");
     	assertTrue(prefs.get("smart_semicolon", "default").equals("false"));
         bot.checkBox("Enable smart semicolon activation").click();
         assertTrue(prefs.get("smart_semicolon", "default").equals("true"));
     }
     
     @Test
-    public void testShadowedVariableWarning() {
+    public void test4_testShadowedVariableWarning() {
     	IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.core");
+    	prefs.put("org.eclipse.jdt.core.compiler.problem.fieldHiding", "ignore");
     	assertTrue(prefs.get("org.eclipse.jdt.core.compiler.problem.fieldHiding", "default").equals("ignore"));
         bot.checkBox("Enable shadowed variable warning").click();
         assertTrue(prefs.get("org.eclipse.jdt.core.compiler.problem.fieldHiding", "default").equals("warning"));
     }
+    
+    @Test
+	public void test5_testMultilineComment() throws Exception {
+		SWTBotEclipseEditor editor = bot.editorByTitle("HelloWorld.java").toTextEditor();
+		
+		
+		editor.setText("public class HelloWorld {\n\n" +
+
+		    "\tpublic static void main(String[] args) {\n" +
+		        "\t\tSystem.out.println(\"Hello\");\n" +
+				"\t\tSystem.out.println(\"World\");\n" +
+		    "\t}\n\n" +
+
+			"}");
+		editor.save();
+		
+		// Comment out two lines
+		editor.insertText(3, 2, "//");
+		editor.save();
+		editor.insertText(4, 2, "//");
+		editor.save();
+	}
+    
+    
+    
+    
+
     
     private static void openJavaPerspective() throws InterruptedException {
 	  Display.getDefault().syncExec(new Runnable() {
@@ -107,4 +149,21 @@ public class UserInterfaceTester {
 		bot.button("Open").click();
 		SWTBotView ideitView = bot.viewByTitle("IDE-IT");
     }
+    
+	private static void createANewJavaProject() throws Exception {
+		bot.menu("File").menu("New").menu("Project...").click();
+		bot.shell("New Project").activate();
+		bot.tree().expandNode("Java").select("Java Project");
+		bot.button("Next >").click();
+		bot.textWithLabel("Project name:").setText("MyFirstProject");
+		bot.button("Finish").click();
+	}
+
+	private static void createANewJavaClass() throws Exception {
+		bot.toolbarDropDownButtonWithTooltip("New Java Class").menuItem("Class").click();
+		bot.shell("New Java Class").activate();
+		bot.textWithLabel("Source folder:").setText("MyFirstProject/src");
+		bot.textWithLabel("Name:").setText("HelloWorld");
+		bot.button("Finish").click();
+	}
 }
