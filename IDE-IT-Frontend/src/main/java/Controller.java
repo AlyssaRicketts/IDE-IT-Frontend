5f142e5989c825ee64c0d26c4f634bf1e5b1c884
Alyssa <alyssr3@cs.washington.edu>
2019-03-11 19:44:40 -0700

    Update hotkey suggestions for proper hotkeys for mac

--- IDE-IT-Frontend/src/main/java/Controller.java
+++ IDE-IT-Frontend/src/main/java/Controller.java
@@ -17,15 +17,18 @@ public class Controller {
 		if (OSInfo.isMac() || OSInfo.isUnix() ) {
 			suggestionsMap.put("blockCommentSuggestion", new Suggestion("blockCommentSuggestion", "Try using 'CMD + /' to comment several lines.", HOTKEY, true));
 			suggestionsMap.put("variableRenameRefactorSuggestion", new Suggestion("variableRenameRefactorSuggestion", "Try using 'CMD + OPTION + R' to rename all instances of a variable, class, or method.", HOTKEY, true));
+			suggestionsMap.put("addImportStatementsSuggestion", new Suggestion("addImportStatementsSuggestion", "Try using 'CMD + SHIFT + O' to add import statements.", HOTKEY, true));
+			suggestionsMap.put("removeUnusedImportStatementsSuggestion", new Suggestion("removeUnusedImportStatementsSuggestion", "Try using 'CMD + SHIFT + O' to remove unused imports.", HOTKEY, true));
+			suggestionsMap.put("correctIndentationsSuggestion", new Suggestion("correctIndentationsSuggestion", "Try using 'CMD + I' to correct indentation.", HOTKEY, true));
 		} else {  // Windows
 			suggestionsMap.put("blockCommentSuggestion", new Suggestion("blockCommentSuggestion", "Try using 'CTRL + SHIFT + /' to comment several lines.", HOTKEY, true));
 			suggestionsMap.put("variableRenameRefactorSuggestion", new Suggestion("variableRenameRefactorSuggestion", "Try using 'ALT + SHIFT + R' to rename all instances of a variable, class, or method.", HOTKEY, true));
+			suggestionsMap.put("addImportStatementsSuggestion", new Suggestion("addImportStatementsSuggestion", "Try using 'CTRL + SHIFT + O' to add import statements.", HOTKEY, true));
+			suggestionsMap.put("removeUnusedImportStatementsSuggestion", new Suggestion("removeUnusedImportStatementsSuggestion", "Try using 'CTRL + SHIFT + O' to remove unused imports.", HOTKEY, true));
+			suggestionsMap.put("correctIndentationsSuggestion", new Suggestion("correctIndentationsSuggestion", "Try using 'CTRL + I' to correct indentation.", HOTKEY, true));
 		}
 
 		// Hotkeys that remain the same between operating systems
-		suggestionsMap.put("addImportStatementsSuggestion", new Suggestion("addImportStatementsSuggestion", "Try using 'CTRL + SHIFT + O' to add import statements.", HOTKEY, true));
-		suggestionsMap.put("removeUnusedImportStatementsSuggestion", new Suggestion("removeUnusedImportStatementsSuggestion", "Try using 'CTRL + SHIFT + O' to remove unused imports.", HOTKEY, true));
-		suggestionsMap.put("correctIndentationsSuggestion", new Suggestion("correctIndentationsSuggestion", "Try using 'CTRL + I' to correct indentation.", HOTKEY, true));
 		suggestionsMap.put("getterSetterSuggestion", new Suggestion("getterSetterSuggestion", "Try using 'ALT + SHIFT + S, R' to automatically generate getters and setters.", HOTKEY, true));
 
 		// Configurations
